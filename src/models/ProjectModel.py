90b0ac986597ff035064914a51c2af3792fb9271
NazzX1 <triomono555@gmail.com>
2024-12-17 21:27:39 +0100

    adding the asset model

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -33,8 +33,9 @@ class ProjectModel(BaseDataModel):
     
     async def create_project(self, project : Project):
 
-        result = self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
-        return result
+        result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
+        project.id = result.inserted_id
+        return project
     
 
     
